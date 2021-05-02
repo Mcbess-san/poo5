3ee9486551075707f445d2f18558c86150e9c137
romain <mcbess18@gmail.com>
2021-05-03 01:07:31 +0200

    add echo's on index

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once 'Bicycle.php';
 
 $bicycle = new Bicycle('blue', 1);
 echo $bicycle->forward();
+echo $bycicle-switchOn();
 var_dump($bicycle);
 
 
@@ -12,4 +13,5 @@ require_once 'Car.php';
 
 $car = new Car('green', 4, 'electric');
 echo $car->forward();
+echo $car->switchOn();
 var_dump(Car::ALLOWED_ENERGIES);
